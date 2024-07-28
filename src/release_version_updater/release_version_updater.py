2defa926dc33ce14b89ac1135f515a525263ed49
Sabiq Khan <sabiq.am.khan@gmail.com>
2024-07-28 03:43:11 -0700

    test: Added unit and integration tests for ReleaseVersionUpdater

--- src/release_version_updater/release_version_updater.py
+++ src/release_version_updater/release_version_updater.py
@@ -14,12 +14,12 @@ from src.clients.github.github_client import GitHubClient
 
 
 class ReleaseVersionUpdater:
-    def __init__(self, logger: Logger, repo_owner: str, repo_name: str, repo_variable: str, github_token: str):
+    def __init__(self, logger: Logger, repo_owner: str, repo_name: str, repo_variable: str, github_client: GitHubClient):
         self.logger: Logger = logger
         self.repo_owner: str = repo_owner
         self.repo_name: str = repo_name
         self.repo_variable: str = repo_variable
-        self.github_client: GitHubClient = GitHubClient(github_token)
+        self.github_client: GitHubClient = github_client
 
     def _get_latest_commit_msg(self) -> str:
         git_log: CompletedProcess = subprocess.run(
@@ -71,26 +71,34 @@ class ReleaseVersionUpdater:
         return new_version
 
     def update_release_version(self):
-        self.logger.info(f"Checking latest commit for {self.repo_owner}/{self.repo_name}...")
-        latest_commit_msg: str = self._get_latest_commit_msg()
-        self.logger.info(f"Latest commit message: {latest_commit_msg}")
-        latest_commit_type: str = self._get_commit_type(latest_commit_msg)
-        self.logger.info(f"Latest commit type: {latest_commit_type}")
+        try:
+            self.logger.info(f"Checking latest commit for {self.repo_owner}/{self.repo_name}...")
+            latest_commit_msg: str = self._get_latest_commit_msg()
+            self.logger.info(f"Latest commit message: {latest_commit_msg}")
+            latest_commit_type: str = self._get_commit_type(latest_commit_msg)
+            self.logger.info(f"Latest commit type: {latest_commit_type}")
 
-        self.logger.info(
-            f"Calling GitHub API to get current release version for {self.repo_owner}/{self.repo_name}...")
-        curr_version: str = self._get_current_version()
-        self.logger.info(f"Latest release version: {curr_version}")
+            self.logger.info(
+                f"Calling GitHub API to get current release version for {self.repo_owner}/{self.repo_name}...")
+            curr_version: str = self._get_current_version()
+            self.logger.info(f"Latest release version: {curr_version}")
 
-        new_version: str = self._increment_version(curr_version, latest_commit_type)
-        self.logger.info(
-            f"Calling GitHub API to update release version for {self.repo_owner}/{self.repo_name} to {new_version}...")
-        response: Response = self.github_client.update_repository_actions_variable(
-            repo_owner=self.repo_owner,
-            repo_name=self.repo_name,
-            variable=self.repo_variable,
-            new_value=new_version
-        )
-        self.logger.info(
-            f"Received the following response from GitHub: {response.status_code}")
-        self.logger.info(f"Successfully incremented release version to {new_version}!")
+            incremented_version: str = self._increment_version(curr_version, latest_commit_type)
+            # Ensures version was successfully incremented
+            if incremented_version != curr_version:
+                self.logger.info(
+                    f"Calling GitHub API to update release version for {self.repo_owner}/{self.repo_name} to {incremented_version}...")
+                response: Response = self.github_client.update_repository_actions_variable(
+                    repo_owner=self.repo_owner,
+                    repo_name=self.repo_name,
+                    variable=self.repo_variable,
+                    new_value=incremented_version
+                )
+                self.logger.info(
+                    f"Received the following response from GitHub: {response.status_code}")
+                self.logger.info(f"Successfully incremented release version from {curr_version} to {incremented_version}!")
+            else:
+                self.logger.info(f"No increment applied to version number {curr_version}. Exiting.")
+        except Exception as e:
+            self.logger.error(e)
+            raise e
--- test/integration/test_release_version_updater.py
+++ test/integration/test_release_version_updater.py
@@ -4,44 +4,52 @@ import unittest
 WORKSPACE_ROOT: str = os.path.abspath(
     os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 sys.path.append(WORKSPACE_ROOT)
+from src.clients.github.github_client import GitHubClient
 from src.release_version_updater.release_version_updater import ReleaseVersionUpdater
 from src.release_version_updater.types import CommitType
 from src.release_version_updater.constants import LOGGER, REPO_OWNER, REPO_NAME, REPO_VARIABLE, GITHUB_TOKEN
 
+EXPECTED_INITIAL_VALUE: str = "v1.0.0"
+
 
 class TestReleaseVersionUpdater(unittest.TestCase):
     def setUp(self):
+        self.github_client: GitHubClient = GitHubClient(github_token=GITHUB_TOKEN)
         self.release_version_updater: ReleaseVersionUpdater = ReleaseVersionUpdater(
             logger=LOGGER,
             repo_owner=REPO_OWNER,
             repo_name=REPO_NAME,
             repo_variable=REPO_VARIABLE,
-            github_token=GITHUB_TOKEN
+            github_client=self.github_client
         )
-    
-    def test_get_commit_type_feature(self):
-        commit_msg: str = "feat: Implemented new API endpoint"
-        commit_type: CommitType = self.release_version_updater._get_commit_type(commit_msg)
 
-        assert commit_type == CommitType.FEATURE
+    def tearDown(self):
+        self.github_client.update_repository_actions_variable(
+            repo_owner=REPO_OWNER,
+            repo_name=REPO_NAME,
+            variable=REPO_VARIABLE,
+            new_value=EXPECTED_INITIAL_VALUE
+        )
 
-    def test_get_commit_type_fix(self):
-        commit_msg: str = "fix: Fixed bug"
-        commit_type: CommitType = self.release_version_updater._get_commit_type(commit_msg)
+    def test_update_release_version(self):
+        curr_version_number: str = self.release_version_updater._get_current_version()
 
-        assert commit_type == CommitType.FIX
+        assert curr_version_number == EXPECTED_INITIAL_VALUE
 
-    def test_get_commit_type_breaking(self):
-        commit_msg: str = "breaking: Deprecated legacy API endpoint"
-        commit_type: CommitType = self.release_version_updater._get_commit_type(commit_msg)
+        self.release_version_updater.update_release_version()
 
-        assert commit_type == CommitType.BREAKING
-    
-    def test_get_commit_type_other(self):
-        commit_msg: str = "Created files"
-        commit_type: CommitType = self.release_version_updater._get_commit_type(commit_msg)
+        latest_commit_msg: str = self.release_version_updater._get_latest_commit_msg()
+        latest_commit_type: str = self.release_version_updater._get_commit_type(latest_commit_msg)
+        updated_version_number: str = self.release_version_updater._get_current_version()
 
-        assert commit_type == CommitType.OTHER
+        if latest_commit_type == CommitType.BREAKING:
+            assert updated_version_number == "v2.0.0"
+        elif latest_commit_type == CommitType.FEATURE:
+            assert updated_version_number == "v1.1.0"
+        elif latest_commit_type == CommitType.FIX:
+            assert updated_version_number == "v1.0.0"
+        elif latest_commit_type == CommitType.OTHER:
+            assert updated_version_number == EXPECTED_INITIAL_VALUE
 
 
 if __name__ == "__main__":
