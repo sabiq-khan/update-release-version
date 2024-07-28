e1db583210fb751a5eef18646fb7ebee2b4d05ce
Sabiq Khan <sabiq.am.khan@gmail.com>
2024-07-28 16:14:34 -0700

    feat: ReleaseVersionUpdater writes to $GITHUB_OUTPUT

--- test/unit/test_release_version_updater.py
+++ test/unit/test_release_version_updater.py
@@ -1,6 +1,8 @@
 import json
 import os
 import sys
+import time
+from typing import List
 import unittest
 from unittest.mock import MagicMock
 from logging import Logger, StreamHandler, Formatter, INFO
@@ -9,7 +11,7 @@ WORKSPACE_ROOT: str = os.path.abspath(
     os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 sys.path.append(WORKSPACE_ROOT)
 from src.release_version_updater.release_version_updater import ReleaseVersionUpdater
-from release_version_updater._types import CommitType
+from src.release_version_updater._types import CommitType
 
 LOGGER: Logger = Logger("test_release_version_updater")
 LOGGER.setLevel(INFO)
@@ -24,18 +26,26 @@ MOCK_REPO_OWNER: str = "test-user"
 MOCK_REPO_NAME: str = "test-repo"
 MOCK_REPO_VARIABLE: str = "VERSION_NUMBER"
 MOCK_GITHUB_TOKEN: str = "abcdefghij-1234567890"
+MOCK_GITHUB_OUTPUT: str = "test-github-output.txt"
 
 
 class TestReleaseVersionUpdater(unittest.TestCase):
     def setUp(self):
         mock_github_client = MagicMock()
+        with open(file=MOCK_GITHUB_OUTPUT, mode="w"):
+            pass
         self.release_version_updater: ReleaseVersionUpdater = ReleaseVersionUpdater(
             logger=LOGGER,
             repo_owner=MOCK_REPO_OWNER,
             repo_name=MOCK_REPO_NAME,
             repo_variable=MOCK_REPO_VARIABLE,
-            github_client=mock_github_client
+            github_client=mock_github_client,
+            github_output=MOCK_GITHUB_OUTPUT
         )
+    
+    def tearDown(self):
+        with open(file=MOCK_GITHUB_OUTPUT, mode="w"):
+            pass
 
     def test_get_commit_type_breaking(self):
         commit_msg: str = "breaking: Deprecated legacy API endpoint"
@@ -122,6 +132,15 @@ class TestReleaseVersionUpdater(unittest.TestCase):
 
         assert new_release_version == "v1.0.0"
 
+    def test_write_to_github_output(self):
+        curr_unix_time: int = int(time.time())
+        self.release_version_updater._write_to_github_output(f"test_key_{curr_unix_time}", f"TEST_VALUE_{curr_unix_time}")
+
+        with open(file=self.release_version_updater.github_output, mode="r") as github_output:
+            lines: List[str] = github_output.readlines()
+        
+        assert lines[-1] == f"test_key_{curr_unix_time}=TEST_VALUE_{curr_unix_time}"
+
     def test_update_release_version(self):
         latest_commit_msg: str = self.release_version_updater._get_latest_commit_msg()
         latest_commit_type: str = self.release_version_updater._get_commit_type(latest_commit_msg)
@@ -140,29 +159,35 @@ class TestReleaseVersionUpdater(unittest.TestCase):
 
         self.release_version_updater.update_release_version()
 
-        if latest_commit_type == CommitType.MAJOR:
-            self.release_version_updater.github_client.update_repository_variable.assert_called_once_with(
-                repo_owner=MOCK_REPO_OWNER,
-                repo_name=MOCK_REPO_NAME,
-                variable=MOCK_REPO_VARIABLE,
-                new_value="v2.0.0"
-            )
-        elif latest_commit_type == CommitType.MINOR:
-            self.release_version_updater.github_client.update_repository_variable.assert_called_once_with(
-                repo_owner=MOCK_REPO_OWNER,
-                repo_name=MOCK_REPO_NAME,
-                variable=MOCK_REPO_VARIABLE,
-                new_value="v1.1.0"
-            )
-        elif latest_commit_type == CommitType.PATCH:
-            self.release_version_updater.github_client.update_repository_variable.assert_called_once_with(
-                repo_owner=MOCK_REPO_OWNER,
-                repo_name=MOCK_REPO_NAME,
-                variable=MOCK_REPO_VARIABLE,
-                new_value="v1.0.1"
-            )
-        elif latest_commit_type == CommitType.OTHER:
-            self.release_version_updater.github_client.update_repository_variable.assert_not_called()
+        with open(file=MOCK_GITHUB_OUTPUT, mode="r") as github_output:
+            lines: List[str] = github_output.readlines()
+            if latest_commit_type == CommitType.MAJOR:
+                self.release_version_updater.github_client.update_repository_variable.assert_called_once_with(
+                    repo_owner=MOCK_REPO_OWNER,
+                    repo_name=MOCK_REPO_NAME,
+                    variable=MOCK_REPO_VARIABLE,
+                    new_value="v2.0.0"
+                )
+                assert lines[-1] == "release_version=v2.0.0"
+            elif latest_commit_type == CommitType.MINOR:
+                self.release_version_updater.github_client.update_repository_variable.assert_called_once_with(
+                    repo_owner=MOCK_REPO_OWNER,
+                    repo_name=MOCK_REPO_NAME,
+                    variable=MOCK_REPO_VARIABLE,
+                    new_value="v1.1.0"
+                )
+                assert lines[-1] == "release_version=v1.1.0"
+            elif latest_commit_type == CommitType.PATCH:
+                self.release_version_updater.github_client.update_repository_variable.assert_called_once_with(
+                    repo_owner=MOCK_REPO_OWNER,
+                    repo_name=MOCK_REPO_NAME,
+                    variable=MOCK_REPO_VARIABLE,
+                    new_value="v1.0.1"
+                )
+                assert lines[-1] == "release_version=v1.0.1"
+            elif latest_commit_type == CommitType.OTHER:
+                self.release_version_updater.github_client.update_repository_variable.assert_not_called()
+                assert lines[-1] == "release_version=v1.0.0"
 
 
 if __name__ == "__main__":
